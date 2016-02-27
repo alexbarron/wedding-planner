867679a9f6c8e6e6070ddb60899ff975a0342b88
alexbarron <alexbarron@gmail.com>
2016-02-27 10:32:34 -0800

    User model and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,63 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do
+  configure do
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get "/" do
     erb :index
+  end
+
+  get "/signup" do
+    if !logged_in?
+      erb :signup
+    else
+      redirect '/', locals: {message: "You're already logged in!"}
+    end
+  end
+
+  post "/signup" do
+    if params[:username] != "" && params[:password] != ""
+      User.create(username: params[:username], email: params[:username], password: params[:password])
+      redirect '/', locals: {message: "Sign up successful, please log in now."}
+    else
+      redirect '/signup', locals: {message: "Sign up failed, try again."}
+    end
+  end
+
+  get "/login" do
+    if !logged_in?
+      erb :login
+    else
+      redirect '/', locals: {message: "You're already logged in!"}
+    end
+  end
+
+  post "/login" do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:id] = user.id
+      redirect '/', locals: {message: "Successfully logged in."}
+    else
+      redirect "/login", locals: {message: "Log in failed, try again."}
+    end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/", locals: {message: "Successfully logged out."}
+  end
+
+  helpers do
+    def logged_in?
+      session[:user_id] ? true : false
+    end
+
+    def current_user
+      User.find(session[:id])
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,3 +10,13 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160227182000) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
+end
