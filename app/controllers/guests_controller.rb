5ec66c3dbd7cb6cd8f5c992c10a3446084978943
alexbarron <alexbarron@gmail.com>
2016-02-27 18:00:23 -0800

    Guests controller

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,14 +1,23 @@
-class WeddingsController < Sinatra::Base
-  set :views, Proc.new { File.join(root, "../views/weddings") }
+class GuestsController < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/guests") }
 
   configure do
     enable :sessions
     set :session_secret, "password_security"
   end
 
-  get '/wedding' do
+  get '/guests' do
     if logged_in?
-      @wedding = current_user.wedding
+      @guests = current_user.wedding.guests
+      erb :index
+    else
+      redirect '/login', locals: {message: "Please log in to see that."}
+    end
+  end
+
+  get '/guests/:id' do
+    if logged_in?
+      @guest = Guest.find(params[:id])
       erb :show
     else
       redirect '/login', locals: {message: "Please log in to see that."}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :weddings
+  has_one :wedding
   has_secure_password
   validates_presence_of :username, :email
 
