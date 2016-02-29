7ba19602d612707b091b91a869941d93470e1d40
alexbarron <alexbarron@gmail.com>
2016-02-28 17:44:12 -0800

    Editing weddings

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -27,4 +27,48 @@ class WeddingsController < ApplicationController
     end
   end
 
+  get '/wedding/new' do
+    if logged_in? && !current_user.wedding
+      erb :'/wedding/new'
+    elsif current_user.wedding
+      redirect '/wedding', locals: {message: "You can't create more than 1 wedding per account."}
+    else
+      redirect "/login", locals: {message: "Please log in to see that."}
+    end
+  end
+
+  post '/wedding' do
+    if logged_in? && params[:name] != ""
+      date = "#{params[:year]}-#{params[:month]}-#{params[:day]}"
+      Wedding.create(name: params[:name], location: params[:location], date: date, user_id: current_user.id)
+      redirect "/wedding"
+    elsif params[:name] == ""
+      redirect "/wedding/new", locals: {message: "Name can't be empty."}
+    else
+      redirect "/login", locals: {message: "Please log in to see that."}
+    end
+  end
+
+  get "/wedding/edit" do
+    if logged_in?
+      @wedding = current_user.wedding
+      erb :'/wedding/edit'
+    else
+      redirect "/login", locals: {message: "Please log in to see that."}
+    end
+  end
+
+  post "/wedding/edit" do
+    if logged_in? && params[:name] != ""
+      @wedding = current_user.wedding
+      date = "#{params[:year]}-#{params[:month]}-#{params[:day]}"
+      @wedding.update(name: params[:name], location: params[:location], date: date)
+      redirect "/wedding"
+    elsif params[:name] == ""
+      redirect "/wedding/edit", locals: {message: "Name can't be empty."}
+    else
+      redirect "/login", locals: {message: "Please log in to see that."}
+    end
+  end
+
 end
